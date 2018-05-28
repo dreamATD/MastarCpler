eb784033817f66993989f3ff4a17550e391d5963
dreamATD <690293245@qq.com>
2018-05-28 11:00:37 +0800

    Almost finish the code generator.

--- codegen.bash
+++ codegen.bash
@@ -7,5 +7,4 @@ set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
 cat > ./example/test.txt   # save everything in stdin to program.txt
-cat ./output/test.asm
 $CCHK
